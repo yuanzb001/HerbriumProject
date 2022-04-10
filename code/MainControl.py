1e0b1570f322863bb8b40d357b0818851d0a54bb
Zhuobin Yuan <>
2022-04-09 23:01:45 -0400

    Build ResNet50 Model

--- code/MainControl.py
+++ code/MainControl.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 from torchvision import transforms
 import os
+import torch
 
 from MyDataLoader import myDataSet
 from DataExtraction import DataExtraction
@@ -9,7 +10,7 @@ from ModelTrainProcess import ModelTrain
 dataUtil = DataExtraction('/pless_nfs/home/yuanzhuobin/project/Herbarium/data/train_metadata.json')
 fileNameList, label_list = dataUtil.getImagePathandLabel()
 
-print(fileNameList)
+#print(fileNameList)
 
 data_transforms = transforms.Compose([
         transforms.RandomResizedCrop(224),
@@ -18,10 +19,10 @@ data_transforms = transforms.Compose([
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-data_dir = '/pless_nfs/home/yuanzhuobin/project/Herbarium/data'
+data_dir = '/pless_nfs/home/yuanzhuobin/project/Herbarium/data/train_images'
 
 image_datasets = myDataSet(data_dir, fileNameList, label_list, data_transforms)
-print(image_datasets)
+#print(image_datasets)
 
-m_modelTrain = ModelTrain(image_datasets)
-m_modelTrain.fitModel()
+m_modelTrain = ModelTrain(image_datasets, ratio = 0.7)
+best_model = m_modelTrain.fitModel()
--- code/ModelTrainProcess.py
+++ code/ModelTrainProcess.py
@@ -3,23 +3,27 @@ from __future__ import print_function, division
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.optim import lr_scheduler
-import torchvision
-from torchvision import models
 import time
 import copy
 
+from ResNet50Model import ResNet50
+
 class ModelTrain():
-    def __init__(self, m_datasets):
+    def __init__(self, m_datasets, ratio):
         self.datasets = m_datasets
+        self.dataset_sizes = {}
+        self.dataloaders = {}
         # self.dataloaders = torch.utils.data.DataLoader(self.datasets, batch_size=4, shuffle=True)
-        self.dataloaders = torch.utils.data.DataLoader(self.datasets, batch_size=4, shuffle=True, num_workers = 4)
-        self.dataset_sizes = len(self.datasets)
+        self.dataset_sizes['train'] = int(len(self.datasets)*ratio)
+        self.dataset_sizes['test'] = int(len(self.datasets)) - int(len(self.datasets)*ratio)
+        self.train_dataset, self.test_dataset = torch.utils.data.random_split(self.datasets,[self.dataset_sizes['train'], self.dataset_sizes['test']], generator=torch.Generator().manual_seed(0))
+        self.dataloaders['train'] = torch.utils.data.DataLoader(self.train_dataset, batch_size=128, shuffle=True, num_workers = 16)
+        self.dataloaders['test'] = torch.utils.data.DataLoader(self.test_dataset, batch_size=32, shuffle=True, num_workers = 16)
         self.class_names = self.datasets.classes
         # self.device = torch.device("cpu")
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
 
-    def singleTrain_model(self, model, criterion, optimizer, scheduler, num_epochs=25):
+    def train_model(self, model, criterion, optimizer, num_epochs=30):
         since = time.time()
 
         best_model_wts = copy.deepcopy(model.state_dict())
@@ -28,40 +32,49 @@ class ModelTrain():
         for epoch in range(num_epochs):
             print(f'Epoch {epoch}/{num_epochs - 1}')
             print('-' * 10)
+            for phase in ['train', 'test']:
+                # Each epoch has a training and test phase
+                if phase == 'train':
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()
 
-            # Each epoch has a training and validation phase
-            model.train()  # Set model to training mode
+                running_loss = 0.0
+                running_corrects = 0
 
-            running_loss = 0.0
-            running_corrects = 0
+                index = 0
+                # Iterate over data.
+                for inputs, labels in self.dataloaders[phase]:
+                    index += 1
+                    inputs = inputs.to(self.device)
+                    labels = torch.tensor(labels).to(self.device)
 
-            # Iterate over data.
-            for inputs, labels in self.dataloaders:
-                inputs = inputs.to(self.device)
-                labels = torch.tensor(labels).to(self.device)
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+                    # forward
+                    # track history if only in train
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
 
-                # forward
-                # track history if only in train
-                outputs = model(inputs)
-                _, preds = torch.max(outputs, 1)
-                loss = criterion(outputs, labels)
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
 
-                # backward + optimize only if in training phase
-                loss.backward()
-                optimizer.step()
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                   
+                epoch_loss = running_loss / self.dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / self.dataset_sizes[phase]
 
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-            scheduler.step()
-
-            epoch_loss = running_loss / self.dataset_sizes
-            epoch_acc = running_corrects.double() / self.dataset_sizes
-
-            print(f'Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+                # deep copy the model
+                if phase == 'test' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    best_model_wts = copy.deepcopy(model.state_dict())
 
         print()
 
@@ -74,21 +87,14 @@ class ModelTrain():
         return model
 
     def fitModel(self):
-        inputs, classes = next(iter(self.dataloaders))
-        out = torchvision.utils.make_grid(inputs)
-        model_ft = models.resnet18(pretrained=True)
-        num_ftrs = model_ft.fc.in_features
-        # Here the size of each output sample is set to 2.
-        # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-        model_ft.fc = nn.Linear(num_ftrs, len(self.class_names))
 
+        model_ft = ResNet50()
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, len(self.class_names))
         model_ft = model_ft.to(self.device)
 
         criterion = nn.CrossEntropyLoss()
         # Observe that all parameters are being optimized
-        optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
-        # Decay LR by a factor of 0.1 every 7 epochs
-        exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-
-        model_ft = self.singleTrain_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
-                                     num_epochs=25)
+        optimizer_ft = optim.Adam(model_ft.parameters(), lr=0.001)
+        model_ft = self.train_model(model_ft, criterion, optimizer_ft,num_epochs=600)
+        return model_ft
